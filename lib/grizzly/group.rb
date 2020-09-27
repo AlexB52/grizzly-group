93de4843c21431891f5c429250ad85b28b11a407
Alexandre Barret <alex@abletech.nz>
2020-09-27 13:34:59 +1300

    Add Grizzly::Group prototype

--- lib/grizzly/group.rb
+++ lib/grizzly/group.rb
@@ -1,2 +1,146 @@
-module Grizzly
+class Grizzly::Group < Array
+  def zip(*args)
+    result = super
+    return result unless result.is_a?(Array)
+
+    result.map {|zipped| new_collection(zipped)}
+  end
+
+  def transpose(*args)
+    new_collection(super)
+  end
+
+  def minmax_by(*args)
+    subgroup(super)
+  end
+
+  def minmax(*args)
+    new_collection(super)
+  end
+
+  def partition(*args)
+    result = super
+    return result unless result.is_a?(Array)
+
+    [new_collection(result[0]), new_collection(result[1])]
+  end
+
+  def group_by(*args)
+    result = super
+    return result unless result.is_a?(Hash)
+
+    result.reduce({}) do |result, (k, v)|
+      result[k] = new_collection(v)
+      result
+    end
+  end
+
+  def sort_by(*args)
+    subgroup(super)
+  end
+
+  def find_all(*args)
+    subgroup(super)
+  end
+
+  def product(*args)
+    result = super
+    return result if is_self?(result)
+    return result unless result.is_a?(Array)
+
+    result.map {|product| new_collection(product)}
+  end
+
+  def rotate(*args)
+    new_collection(super)
+  end
+
+  def compact(*args)
+    new_collection(super)
+  end
+
+  def shuffle(*args)
+    new_collection(super)
+  end
+
+  def sort(*args)
+    new_collection(super)
+  end
+
+  def reverse(*args)
+    new_collection(super)
+  end
+
+  def reject(*args)
+    subgroup(super)
+  end
+
+  def filter(*args)
+    subgroup(super)
+  end
+
+  def select(*args)
+    subgroup(super)
+  end
+
+  def values_at(*args)
+    new_collection(super)
+  end
+
+  def first(*args)
+    subgroup(super)
+  end
+
+  def last(*args)
+    subgroup(super)
+  end
+
+  def &(*args)
+    new_collection(super)
+  end
+
+  def |(*args)
+    new_collection(super)
+  end
+
+  def +(*args)
+    new_collection(super)
+  end
+
+  def union(*args)
+    new_collection(super)
+  end
+
+  def -(*args)
+    new_collection(super)
+  end
+
+  def difference(*args)
+    new_collection(super)
+  end
+
+  def pop(*args)
+    subgroup(super)
+  end
+
+  def shift(*args)
+    subgroup(super)
+  end
+
+  private
+
+  def subgroup(result)
+    return result unless result.is_a?(Array)
+    return result if is_self?(result)
+
+    new_collection(result)
+  end
+
+  def new_collection(array)
+    self.class.new(array)
+  end
+
+  def is_self?(obj)
+    obj.object_id.eql? object_id
+  end
 end
