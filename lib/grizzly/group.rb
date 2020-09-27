0cd3b68db24222bfa73bb00bb271b22f71d54772
Alexandre Barret <alex@abletech.nz>
2020-09-27 13:18:28 +1300

    First commit

---
+++ lib/grizzly/group.rb
@@ -0,0 +1,8 @@
+require "grizzly/group/version"
+
+module Grizzly
+  module Group
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ test/grizzly/group_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class Grizzly::GroupTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Grizzly::Group::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
