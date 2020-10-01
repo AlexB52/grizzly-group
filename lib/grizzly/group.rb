b80a86d56f6d11cc7b66f98e622973a3e241f91e
Alexandre Barret <alex@abletech.nz>
2020-10-01 17:51:15 +1300

    Attempt to metaprogram those Enumerable & Enumerator methods

--- lib/grizzly/group.rb
+++ lib/grizzly/group.rb
@@ -154,13 +154,13 @@ end
 class Grizzly::Enumerator < Enumerator
   extend Forwardable
 
-  METHODS = %i{+ each feed inspect next next_values peek peek_values rewind size with_index with_object}
-  # each_with_index each_with_object
-  ENUMERABLE_METHODS = %i{all? any? chain chunk chunk_while collect collect_concat count cycle detect drop drop_while each_cons each_entry each_slice each_with_index each_with_object entries filter filter_map find find_all find_index first flat_map grep grep_v group_by include? inject lazy map max max_by member? min min_by minmax minmax_by none? one? partition reduce reject reverse_each select slice_after slice_before slice_when sort sort_by sum take take_while tally to_a to_h uniq zip}
-  #filter_map tally
+  OVERRIDE_METHODS = %i{with_index with_object each_with_index each_with_object}
+  ENUMERATOR_METHODS = Enumerator.public_instance_methods(false)
+  ENUMERABLE_METHODS = Enumerable.public_instance_methods
+
   alias :old_enum :to_enum
 
-  def_delegators :@enum, *METHODS
+  def_delegators :@enum, *ENUMERATOR_METHODS
 
   ENUMERABLE_METHODS.each do |method_name|
     define_method(method_name) do |*args, &block|
@@ -170,7 +170,16 @@ class Grizzly::Enumerator < Enumerator
     end
   end
 
+  # OVERRIDE_METHODS.each do |method_name|
+  #   define_method(method_name) do |*args, &block|
+  #     return to_enum(__method__, *args) unless block
+
+  #     @enum.send(:each, *args, &block)
+  #   end
+  # end
+
   attr_reader :object
+
   def initialize(object, method_name, *args)
     @object = object
     @enum = object.to_enum(method_name, *args)
