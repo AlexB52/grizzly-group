9e0f43b5121d94d8e6fc2caf14e14aec352d204e
Alexandre Barret <alex@abletech.nz>
2020-10-07 18:45:34 +1300

    Grizzly::Enumerator#first

--- spec/grizzly/group/enumerator/first_spec.rb
+++ spec/grizzly/group/enumerator/first_spec.rb
@@ -1,7 +1,9 @@
-# require_relative '../../../spec_helper'
+require_relative '../../../spec_helper'
 
-# describe "Enumerator#first" do
-#   it "returns arrays correctly when calling #first (2376)" do
-#     Enumerator.new {|y| y << [42] }.first.should == [42]
-#   end
-# end
+describe "Grizzly::Enumerator#first" do
+  before { @subject = Grizzly::Enumerator }
+
+  it "returns arrays correctly when calling #first (2376)" do
+    @subject.new(Enumerator.new {|y| y << [42] }).first.should == [42]
+  end
+end
