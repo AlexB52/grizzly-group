fba101a578e3a73d3c8b2fbc49abbca7b43b6f62
Alexandre Barret <alex@abletech.nz>
2020-10-08 16:50:23 +1300

    convert inspect_spec

--- spec/grizzly/group/enumerator/inspect_spec.rb
+++ spec/grizzly/group/enumerator/inspect_spec.rb
@@ -1,19 +1,19 @@
-# require_relative '../../../spec_helper'
+require_relative '../../../spec_helper'
 
-# describe "Grizzly::Enumerator#inspect" do
-#   before { @subject = Grizzly::Enumerator }
+describe "Grizzly::Enumerator#inspect" do
+  before { @subject = Grizzly::Enumerator }
 
-#   describe "shows a representation of the Enumerator" do
-#     it "including receiver and method" do
-#       @subject.new((1..3).each).inspect.should == "#<Grizzly::Enumerator: 1..3:each>"
-#     end
+  describe "shows a representation of the Enumerator" do
+    it "including receiver and method" do
+      @subject.new((1..3), :each).inspect.should == "#<Grizzly::Enumerator: 1..3:each>"
+    end
 
-#     it "including receiver and method and arguments" do
-#       @subject.new((1..3).each_slice(2)).inspect.should == "#<Grizzly::Enumerator: 1..3:each_slice(2)>"
-#     end
+    it "including receiver and method and arguments" do
+      @subject.new((1..3), :each_slice, nil, 2).inspect.should == "#<Grizzly::Enumerator: 1..3:each_slice(2)>"
+    end
 
-#     it "including the nested Enumerator" do
-#       @subject.new((1..3).each.each_slice(2)).inspect.should == "#<Grizzly::Enumerator: #<Grizzly::Enumerator: 1..3:each>:each_slice(2)>"
-#     end
-#   end
-# end
+    it "including the nested Enumerator" do
+      @subject.new((1..3).each, :each_slice, nil, 2).inspect.should == "#<Grizzly::Enumerator: #<Grizzly::Enumerator: 1..3:each>:each_slice(2)>"
+    end
+  end
+end
