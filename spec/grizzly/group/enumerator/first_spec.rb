156a91b9fb098abe3b78da3614857988e345dcb4
Alexandre Barret <alex@abletech.nz>
2020-10-08 16:47:57 +1300

    convert first_spec

--- spec/grizzly/group/enumerator/first_spec.rb
+++ spec/grizzly/group/enumerator/first_spec.rb
@@ -1,9 +1,9 @@
-# require_relative '../../../spec_helper'
+require_relative '../../../spec_helper'
 
-# describe "Grizzly::Enumerator#first" do
-#   before { @subject = Grizzly::Enumerator }
+describe "Grizzly::Enumerator#first" do
+  before { @subject = Grizzly::Enumerator }
 
-#   it "returns arrays correctly when calling #first (2376)" do
-#     @subject.new(Enumerator.new {|y| y << [42] }).first.should == [42]
-#   end
-# end
+  it "returns arrays correctly when calling #first (2376)" do
+    @subject.new(Enumerator.new {|y| y << [42] }).first.should == [42]
+  end
+end
