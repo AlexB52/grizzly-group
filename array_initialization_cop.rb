f7a3a7a1c78e113ca6a06bb653880d61e2ee616f
Alex Barret <alex.barret@veridapt.com>
2022-10-09 21:19:18 +1300

    add rubocop rule

---
+++ array_initialization_cop.rb
@@ -0,0 +1,37 @@
+require "rubocop"
+
+module RuboCop
+  module Cop
+    module Style
+      class ArrayInitialization < Base
+        extend RuboCop::Cop::AutoCorrector
+
+        ERROR_MSG = "an Array should not be initialized with the literal constructor []"
+
+        def_node_matcher :array_literal, <<~PATTERN
+          (array $...)
+        PATTERN
+
+        def_node_matcher :array_create, <<~PATTERN
+          (send (const ...) :new (array ...))
+        PATTERN
+
+        def_node_matcher :array_spec_matched, <<~PATTERN
+          (send (send ...) :== (array ...))
+        PATTERN
+
+        def on_array(node)
+          expression = array_literal(node)
+          return unless expression
+          return if array_create(node.parent)
+          return if array_spec_matched(node.parent)
+
+          add_offense(node, message: ERROR_MSG) do |corrector|
+            constant_to_replace = cop_config['InitializeArrayWith']
+            corrector.replace(node, "#{constant_to_replace}.new(#{node.source})")
+          end
+        end
+      end
+    end
+  end
+end
