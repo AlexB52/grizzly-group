71e4f25f53e2f2713e187feaac878c550b086480
Alexandre Barret <barret.alx@gmail.com>
2022-12-04 13:06:25 +1300

    Cleanup skipped tests

--- spec/skipped_tests/core/array/new_spec.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../../spec_helper'
-
-describe "Array.new" do
-  # core/array/new_spec.rb:5
-  it "returns an instance of Array" do
-    Grizzly::Collection.new.should be_an_instance_of(Grizzly::Collection)
-  end
-end
