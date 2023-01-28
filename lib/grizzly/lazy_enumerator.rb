9332a3e0405cbce17af987966726750091d8d8a9
Alexandre Barret <barret.alx@gmail.com>
2023-01-28 22:26:41 +1300

    Reactor lazy overrides

--- lib/grizzly/lazy_enumerator.rb
+++ lib/grizzly/lazy_enumerator.rb
@@ -2,6 +2,41 @@ module Grizzly
   class LazyEnumerator
     include ::Grizzly::Enumerable
 
+    %i[
+      select
+      chunk
+      chunk_while
+      collect
+      collect_concat
+      compact
+      drop
+      drop_while
+      eager
+      enum_for
+      filter
+      filter_map
+      find_all
+      flat_map
+      force
+      grep
+      grep_v
+      map
+      reject
+      slice_after
+      slice_before
+      slice_when
+      take
+      take_while
+      to_enum
+      uniq
+      with_index
+      zip
+    ].each do |method_name|
+      define_method(method_name) do |*args, &block|
+        subgroup @obj.send(method_name, *args, &block)
+      end
+    end
+
     attr_reader :obj, :instantiating_class
     def initialize(obj, instantiating_class: Array)
       @obj = obj
@@ -27,120 +62,6 @@ module Grizzly
 
     def to_a(*args, &block)
       @obj.to_a(*args, &block) # no subgroup?
-    end
-
-    # Lazy override
-
-    def select(*args, &block)
-      subgroup @obj.select(*args, &block)
-    end
-
-    def chunk(*args, &block)
-      subgroup @obj.chunk(*args, &block)
-    end
-
-    def chunk_while(*args, &block)
-      subgroup @obj.chunk_while(*args, &block)
-    end
-
-    def collect(*args, &block)
-      subgroup @obj.collect(*args, &block)
-    end
-
-    def collect_concat(*args, &block)
-      subgroup @obj.collect_concat(*args, &block)
-    end
-
-    def compact(*args, &block)
-      subgroup @obj.compact(*args, &block)
-    end
-
-    def drop(*args, &block)
-      subgroup @obj.drop(*args, &block)
-    end
-
-    def drop_while(*args, &block)
-      subgroup @obj.drop_while(*args, &block)
-    end
-
-    def eager(*args, &block)
-      subgroup @obj.eager(*args, &block)
-    end
-
-    def enum_for(*args, &block)
-      subgroup @obj.enum_for(*args, &block)
-    end
-
-    def filter(*args, &block)
-      subgroup @obj.filter(*args, &block)
-    end
-
-    def filter_map(*args, &block)
-      subgroup @obj.filter_map(*args, &block)
-    end
-
-    def find_all(*args, &block)
-      subgroup @obj.find_all(*args, &block)
-    end
-
-    def flat_map(*args, &block)
-      subgroup @obj.flat_map(*args, &block)
-    end
-
-    def force(*args, &block)
-      subgroup @obj.force(*args, &block)
-    end
-
-    def grep(*args, &block)
-      subgroup @obj.grep(*args, &block)
-    end
-
-    def grep_v(*args, &block)
-      subgroup @obj.grep_v(*args, &block)
-    end
-
-    def map(*args, &block)
-      subgroup @obj.map(*args, &block)
-    end
-
-    def reject(*args, &block)
-      subgroup @obj.reject(*args, &block)
-    end
-
-    def slice_after(*args, &block)
-      subgroup @obj.slice_after(*args, &block)
-    end
-
-    def slice_before(*args, &block)
-      subgroup @obj.slice_before(*args, &block)
-    end
-
-    def slice_when(*args, &block)
-      subgroup @obj.slice_when(*args, &block)
-    end
-
-    def take(*args, &block)
-      subgroup @obj.take(*args, &block)
-    end
-
-    def take_while(*args, &block)
-      subgroup @obj.take_while(*args, &block)
-    end
-
-    def to_enum(*args, &block)
-      subgroup @obj.to_enum(*args, &block)
-    end
-
-    def uniq(*args, &block)
-      subgroup @obj.uniq(*args, &block)
-    end
-
-    def with_index(*args, &block)
-      subgroup @obj.with_index(*args, &block)
-    end
-
-    def zip(*args, &block)
-      subgroup @obj.zip(*args, &block)
     end
   end
 end
